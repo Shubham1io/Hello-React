d1bd23ed2d79d7e6bbf5617f6d89272b6d71282d
jknkkm <kjkknknkn>
2025-11-02 13:30:36 +0530

    Updates

---
+++ Grocery.582a9cca.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire2d53;(0,e.register)("agtsM",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),t=()=>(0,i.jsx)("h1",{children:"This is grocery component and has alot of child component inside it."})});
+//# sourceMappingURL=Grocery.582a9cca.js.map
